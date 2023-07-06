bd312ff321b4b6c4392d76005b82d02796e3c6e3
luis <lflores@vozy.co>
2023-07-06 08:38:41 -0500

    fix problems

--- src/auth/constants.js
+++ src/auth/constants.js
@@ -1,2 +1,2 @@
-export const doaminAuth0 = process.env.REACT_APP_DOMAIN_AUTH0
-export const clientIdAuth0 = process.env.REACT_APP_CLIENT_ID
+export const doaminAuth0 = process.env.REACT_APP_DOMAIN_AUTH0;
+export const clientIdAuth0 = process.env.REACT_APP_CLIENT_ID;
